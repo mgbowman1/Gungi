68edb29233c737e3bae58a1a2bbb01a306af94f6
Michael Bowman <mgbowman3@gmail.com>
2017-04-25 22:54:27 -0500

    Updated Framework

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from Pieces import *
+
 currentTurn: bool = True  # variable for tracking whose turn it is, true means white and false means black
-WhiteHand: list = []  # the starting hand of the white team
-BlackHand: list = []  # the starting hand of the black team
+whiteHand: list = []  # the starting hand of the white team
+blackHand: list = []  # the starting hand of the black team
+boardPieces: list = []  # the list of pieces that are currently on the board
 moves: list = []  # list of all moves made so far
 
 def getInput() -> str:  # receives and returns string input
 
 def printScreen():  # prints out the board and other necessities
+
+def getPiece(type: Piece, p: Position) -> Piece:  # searches through the boardPieces list and find the piece specified by the given class type and position on the board
+
+def main():  # main function which executes functions
+
+main()  # calls the main function to begin and end the program
